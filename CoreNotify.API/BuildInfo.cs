f10708d6ccceb24762e3404445085db9ef3f0e31
Adam O'Neil <adamosoftware@gmail.com>
2025-01-20 07:21:33 -0500

    info endpoint

---
+++ CoreNotify.API/BuildInfo.cs
@@ -0,0 +1,8 @@
+﻿using Pekspro.BuildInformationGenerator;
+
+namespace CoreNotify.API;
+
+[BuildInformation(AddGitCommitId = true, AddLocalBuildTime = true, FakeIfDebug = false, FakeIfRelease = false, AddGitBranch = true)]
+partial class BuildInfo
+{
+}
---
+++ CoreNotify.API/Controllers/InfoController.cs
@@ -0,0 +1,23 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreNotify.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InfoController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult Get()
+	{
+		return Ok(new
+		{
+			BuildInfo = new
+			{
+				BuildInfo.LocalBuildTime,
+				BuildInfo.Git.ShortCommitId,
+				BuildAge = BuildInfo.LocalBuildTime.Humanize()
+			}
+		});
+	}
+}
