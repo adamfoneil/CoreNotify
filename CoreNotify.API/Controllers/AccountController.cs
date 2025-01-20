5955234947d899b5d4475012a29b3dd4b313e6c3
Adam O'Neil <adamosoftware@gmail.com>
2025-01-20 11:47:22 -0500

    reworked html

--- CoreNotify.API/Controllers/AccountController.cs
+++ CoreNotify.API/Controllers/AccountController.cs
@@ -35,8 +35,10 @@ public class AccountController(
 			{
 				To = [request.Email],
 				Subject = "CoreNotify Account Created",
-				Html = $"<p>Your account has been created with a renewal date of {account.RenewalDate:M/d/yy}. Your API key is: <strong>{account.ApiKey}</strong></p><p>If you did not do this, please ignore. Someone entered your email by mistake.</p>" +
-				"<p>Please see <a href=\"https://github.com/adamfoneil/CoreNotify\">https://github.com/adamfoneil/CoreNotify</a> for more info.</p>"
+				Html = 
+					$"<p>Your account has been created with a renewal date of {account.RenewalDate:M/d/yy}. Your API key is: <strong>{account.ApiKey}</strong></p>" +
+					$"<p>If you did not do this, please ignore. Someone entered your email by mistake.</p>" +
+					"<p>Please see <a href=\"https://github.com/adamfoneil/CoreNotify\">https://github.com/adamfoneil/CoreNotify</a> for more info.</p>"
 			});
 			
 			_logger.LogInformation("Account created for {email}", request.Email);
