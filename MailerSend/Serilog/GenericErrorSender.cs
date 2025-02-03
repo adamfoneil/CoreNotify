24ebd4d76e604c74489670e725c6575c8f41ccd1
Adam O'Neil <adamosoftware@gmail.com>
2025-02-03 07:02:47 -0500

    started on Serilog stuff

---
+++ MailerSend/Serilog/GenericErrorSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MailerSend.Serilog;
+
+public class GenericErrorSender<TKey>(
+	MailerSendClient mailerSendClient,
+	ILogger<GenericErrorSender<TKey>> logger,
+	SinkData<TKey> serilogData) where TKey : notnull
+{
+	private readonly MailerSendClient _sendClient = mailerSendClient;
+	private readonly ILogger<GenericErrorSender<TKey>> _logger = logger;
+	private readonly SinkData<TKey> _serilogData = serilogData;
+}
---
+++ MailerSend/Serilog/SinkData.cs
@@ -0,0 +1,26 @@
+﻿namespace MailerSend.Serilog;
+
+public abstract class SinkData<TKey> where TKey : notnull
+{
+	public abstract Task<IEnumerable<LogEntry<TKey>>> QueryEntriesAsync();
+	public abstract Task MarkEntriesAsync(IEnumerable<TKey> entryIds);
+	
+	protected abstract Dictionary<string, object?> ParseProperties(string? properties);
+
+	public async Task<(IEnumerable<LogEntry<TKey>> Entries, Dictionary<TKey, Dictionary<string, object?>> Properties)> QueryAsync()
+	{
+		var newEntries = await QueryEntriesAsync();
+		var properties = newEntries.ToDictionary(row => row.Id, row => ParseProperties(row.Properties));
+		return (newEntries, properties);
+	}
+}
+
+public class LogEntry<TKey> where TKey : notnull
+{
+	public TKey Id { get; set; } = default!;
+	public DateTime Timestamp { get; set; }
+	public string Message { get; set; } = default!;
+	public string Level { get; set; } = default!;
+	public string? Exception { get; set; }
+	public string? Properties { get; set; }
+}
