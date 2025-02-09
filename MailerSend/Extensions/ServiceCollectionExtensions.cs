c0f3511ee0a8cc0e8e7800ba1805cedaf920ca86
Adam O'Neil <adamosoftware@gmail.com>
2025-02-09 06:53:09 -0500

    v1.0.4 generic email sender ext method

--- MailerSend/Extensions/ServiceCollectionExtensions.cs
+++ MailerSend/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MailerSend.Extensions;
 
@@ -14,5 +15,24 @@ public static class ServiceCollectionExtensions
 		services.Configure<CoreNotifyOptions>(configuration.GetSection("CoreNotify"));
 		services.AddHttpClient();
 		services.AddSingleton<IEmailSender<TUser>, CoreNotifyEmailSender<TUser>>();
+	}
+
+	public static void AddCoreNotifyGenericEmailSender<TUser>(this IServiceCollection services, string domain, IConfiguration? config = null, string? mailerConfigSection = null) where TUser : IdentityUser
+	{
+		services.AddHttpClient();
+
+		if (config is not null)
+		{
+			services.Configure<MailerSendOptions>(config.GetSection(mailerConfigSection ?? "MailerSend"));
+		}
+
+		services.AddSingleton<MailerSendClient>();
+
+		services.AddSingleton<IEmailSender<TUser>, GenericEmailSender<TUser>>(
+			sp => new GenericEmailSender<TUser>(
+				domain,
+				sp.GetRequiredService<MailerSendClient>(),
+				sp.GetRequiredService<ILogger<GenericEmailSender<TUser>>>()
+				));
 	}
 }
