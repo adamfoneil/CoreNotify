0e85a34a922a804fab12f356831580a8a64c5a4b
Adam O'Neil <adamosoftware@gmail.com>
2025-02-16 11:49:58 -0500

    extracted filter helper to shared project

---
+++ CoreNotify.SerilogAlerts.Shared/FilterHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace CoreNotify.SerilogAlerts.Shared;
+
+public static class FilterHelper
+{
+	public static bool ExcludeByProperty(
+		IEnumerable<KeyValuePair<string, string>> excludeProperties,
+		Dictionary<string, object?> entryProperties)
+	{
+		foreach (var (EntryValue, CriteriaValue) in excludeProperties.Join(
+			entryProperties, kp => kp.Key, kp => kp.Key,
+			(criteria, entry) => (EntryValue: entry.Value?.ToString() ?? string.Empty, CriteriaValue: criteria.Value), StringComparer.OrdinalIgnoreCase))
+		{
+			if (EntryValue.Contains(CriteriaValue, StringComparison.OrdinalIgnoreCase)) return true;
+		}
+
+		return false;
+	}
+}
--- CoreNotify.SerilogAlerts.SqlServer/SqlServerSerilogQuery.cs
+++ CoreNotify.SerilogAlerts.SqlServer/SqlServerSerilogQuery.cs
@@ -74,21 +74,7 @@ public class SqlServerSerilogQuery(
 			if (entry.MessageTemplate.Contains(excludeTemplate, StringComparison.OrdinalIgnoreCase)) return true;
 		}
 
-		if (ExcludeByProperty(_options.ExcludeProperties, entry.PropertyDictionary)) return true;
-
-		return false;
-	}
-
-	public static bool ExcludeByProperty(
-		IEnumerable<KeyValuePair<string, string>> excludeProperties,
-		Dictionary<string, object?> entryProperties)
-	{
-		foreach (var (EntryValue, CriteriaValue) in excludeProperties.Join(
-			entryProperties, kp => kp.Key, kp => kp.Key,
-			(criteria, entry) => (EntryValue: entry.Value?.ToString() ?? string.Empty, CriteriaValue: criteria.Value), StringComparer.OrdinalIgnoreCase))
-		{
-			if (EntryValue.Contains(CriteriaValue, StringComparison.OrdinalIgnoreCase)) return true;
-		}
+		if (FilterHelper.ExcludeByProperty(_options.ExcludeProperties, entry.PropertyDictionary)) return true;
 
 		return false;
 	}
