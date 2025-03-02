0ff94510444782f7c89acad90783e0bf10cd5cc0
Adam O'Neil <adamosoftware@gmail.com>
2025-03-02 14:41:33 -0500

    started on log query endpoint

--- CoreNotify.API/Controllers/AdminController.cs
+++ CoreNotify.API/Controllers/AdminController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreNotify.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Services.Data;
 
 namespace CoreNotify.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 [VerifyAccount(requireAdmin: true)]
-public class AdminController : Controller
+public class AdminController(IDbContextFactory<ApplicationDbContext> dbFactory) : Controller
 {
+	private readonly IDbContextFactory<ApplicationDbContext> _dbFactory = dbFactory;
+
+	public async Task<IActionResult> Logs(LogsRequest request)
+	{
+		throw new NotImplementedException();
+	}
 }
---
+++ CoreNotify.Shared/Models/LogsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreNotify.Shared.Models;
+
+public class LogsRequest
+{
+	public string? AgeInterval { get; set; }
+	public string? SourceContext { get; set; }
+	public string? MessageTemplate { get; set; }
+}
