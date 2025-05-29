98a67c5434ce955bcef652c9b5d64f07e1e4ccee
Adam O'Neil <adamosoftware@gmail.com>
2025-05-29 07:10:04 -0400

    #6 new debug endpoint

--- CoreNotify.API/Controllers/InfoController.cs
+++ CoreNotify.API/Controllers/InfoController.cs
@@ -1,24 +1,28 @@
 ﻿using Humanizer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Services;
 
 namespace CoreNotify.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class InfoController : ControllerBase
+public class InfoController(IOptions<ExpirationReminder.Options> expirationReminderOptions) : ControllerBase
 {
+	private readonly ExpirationReminder.Options _expirationReminderOptions = expirationReminderOptions.Value;
+
 	[HttpGet]
-	public IActionResult Get()
+	public IActionResult Get() => Ok(new
 	{
-		return Ok(new
+		BuildInfo = new
 		{
-			BuildInfo = new
-			{
-				LocalTime = BuildInfo.LocalBuildTime,
-				BuildInfo.Git.ShortCommitId,
-				Age = BuildInfo.LocalBuildTime.Humanize(),
-				Version = VersionReader.Value
-			}
-		});
-	}
+			LocalTime = BuildInfo.LocalBuildTime,
+			BuildInfo.Git.ShortCommitId,
+			Age = BuildInfo.LocalBuildTime.Humanize(),
+			Version = VersionReader.Value
+		}
+	});
+
+	[HttpGet("AccountReminders")]
+	public IActionResult AccountReminders() => Ok(_expirationReminderOptions);	
 }
