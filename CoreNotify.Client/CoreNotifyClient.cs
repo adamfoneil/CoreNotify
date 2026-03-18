899663112a4ff68c8c0d58d59395633e921ecfa4
adamfoneil <adamfoneil@proton.me>
2026-03-18 05:53:43 -0400

    CoreNotify.MailerSend v1.0.14 added logging

--- CoreNotify.Client/CoreNotifyClient.cs
+++ CoreNotify.Client/CoreNotifyClient.cs
@@ -1,18 +1,24 @@
 ﻿using CoreNotify.Shared;
 using CoreNotify.Shared.Models;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace CoreNotify.Client;
 
-public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Options> options)
+public class CoreNotifyClient(
+	ILogger<CoreNotifyClient> logger,
+    IHttpClientFactory httpClientFactory, 
+	IOptions<Options> options)
 {
-	private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+    private readonly ILogger<CoreNotifyClient> _logger = logger;
+    private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
 	private readonly Options _options = options.Value;
 
 	public async Task CreateAccountAsync(string accountEmail)
 	{
+		_logger.LogDebug("Creating account for {accountEmail}", accountEmail);
 		var client = GetHttpClient();
 		var response = await client.PostAsJsonAsync("api/account/register", new CreateAccountRequest { Email = accountEmail });
 		await response.ThrowIfProblemResponseAsync();
@@ -20,14 +26,16 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 
 	public async Task ResendApiKeyAsync(string accountEmail)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Resending API key for {accountEmail}", accountEmail);
+        var client = GetHttpClient();
 		var response = await client.PostAsJsonAsync("api/account/resend", new CreateAccountRequest() { Email = accountEmail });
 		await response.ThrowIfProblemResponseAsync();
 	}
 
 	public async Task<AccountUsageResponse> GetUsageAsync(string accountEmail, string apiKey)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Getting usage for {accountEmail}", accountEmail);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.GetAsync("api/account/usage");		
 		await response.ThrowIfProblemResponseAsync();
@@ -36,7 +44,8 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 
 	public async Task<string> SendConfirmationAsync(string accountEmail, string apiKey, SendConfirmationRequest request)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Sending confirmation email for {accountEmail} to {email}", accountEmail, request.Email);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.PostAsJsonAsync("api/send/confirmation", request);
 		await response.ThrowIfProblemResponseAsync();
@@ -45,7 +54,8 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 	
 	public async Task<string> SendResetLinkAsync(string accountEmail, string apiKey, SendResetLinkRequest request)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Sending password reset link email for {accountEmail} to {email}", accountEmail, request.Email);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.PostAsJsonAsync("api/send/resetlink", request);
 		await response.ThrowIfProblemResponseAsync();
@@ -54,7 +64,8 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 
 	public async Task<string> SendResetCodeAsync(string accountEmail, string apiKey, SendResetCodeRequest request)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Sending password reset code email for {accountEmail} to {email}", accountEmail, request.Email);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.PostAsJsonAsync("api/send/resetcode", request);
 		await response.ThrowIfProblemResponseAsync();
@@ -63,7 +74,8 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 
 	public async Task<string> RecycleKeyAsync(string accountEmail, string apiKey)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Recycling API key for {accountEmail}", accountEmail);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.PostAsJsonAsync("api/account/recycle", new CreateAccountRequest() { Email = accountEmail });
 		await response.ThrowIfProblemResponseAsync();
@@ -72,7 +84,8 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 
 	public async Task SendAlertAsync(string accountEmail, string apiKey, SendAlertRequest request)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Sending alert for {accountEmail} to {email}", accountEmail, request.Email);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.PostAsJsonAsync("api/send/alert", request);
 		await response.ThrowIfProblemResponseAsync();
@@ -80,7 +93,8 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 
 	public async Task<long> GetContinuationMarkerAsync(string accountEmail, string apiKey, string name)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Getting continuation marker {name} for {accountEmail}", name, accountEmail);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.GetAsync($"api/marker/{name}");
 		await response.ThrowIfProblemResponseAsync();
@@ -91,7 +105,8 @@ public class CoreNotifyClient(IHttpClientFactory httpClientFactory, IOptions<Opt
 
 	public async Task SetContinuationMarker(string accountEmail, string apiKey, string name, long value)
 	{
-		var client = GetHttpClient();
+		_logger.LogDebug("Setting continuation marker {name} to {value} for {accountEmail}", name, value, accountEmail);
+        var client = GetHttpClient();
 		client.AddAuthorization(accountEmail, apiKey);
 		var response = await client.PutAsync($"api/marker/{name}/{value}", new StringContent(""));
 		await response.ThrowIfProblemResponseAsync();
